dce92ac8244135dbdecd4727b82446a100bb6f04
mato1370 <96350246+mato1370@users.noreply.github.com>
2021-12-21 01:59:10 +0900

    パブリックにお引越し

---
+++ public/js/nav.js
@@ -0,0 +1,9 @@
+function getNavigation() {
+	let		html = "";
+	html += '<ul>';
+	html += '	<li> <a href="https://trpfrog.net/" target="_blank">Vlog</a></li>';
+	html += '	<li> <a href="https://library.azabu-u.ac.jp/azlib/ms/" target="_blank">Vlog</a></li>';
+	html += '	<li> <a href="https://twitter.com/mato1370" target="_blank">Vlog</a></li>';
+	html += '</ul>';
+	document.write( html );
+}
