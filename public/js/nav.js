1327b8250f802c38363889f5d31017c029621cae
mato1370 <96350246+mato1370@users.noreply.github.com>
2021-12-21 03:02:53 +0900

    Link 変更

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,9 +1,9 @@
 function getNavigation() {
 	let		html = "";
 	html += '<ul>';
-	html += '	<li> <a href="https://trpfrog.net/" target="_blank">Vlog</a></li>';
-	html += '	<li> <a href="https://twitter.com/mato1370" target="_blank">Vlog</a></li>';
-	html += '	<li> <a href="https://twitter.com/mato1370" target="_blank">Vlog</a></li>';
+	html += '	<li> <a href="https://trpfrog.net/" target="_blank">つまみネット</a></li>';
+	html += '	<li> <a href="https://negiissei.com/links.html" target="_blank">negiissei-links</a></li>';
+	html += '	<li> <a href="https://twitter.com/mato1370" target="_blank">Twtter</a></li>';
 	html += '</ul>';
 	document.write( html );
 }
