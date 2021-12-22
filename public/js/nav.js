002c94a8eacb1af8c7eda8f8d3e2cb33850a7008
mato1370 <96350246+mato1370@users.noreply.github.com>
2021-12-22 22:10:31 +0900

    Blog　追加

--- public/js/nav.js
+++ public/js/nav.js
@@ -4,6 +4,7 @@ function getNavigation() {
 	html += '	<li> <a href="https://trpfrog.net/" target="_blank">つまみネット</a></li>';
 	html += '	<li> <a href="https://negiissei.com/links.html" target="_blank">negiissei-links</a></li>';
 	html += '	<li> <a href="https://twitter.com/mato1370" target="_blank">Twtter</a></li>';
+	html += '	<li> <a href="Blog.html" target="_blank">Blog</a></li>';
 	html += '</ul>';
 	document.write( html );
 }
